5c01bbfa3fcd1ee313db05adad9d39b9819f9189
Ahmad Noor <NourCS@hotmail.com>
2020-11-29 22:24:48 -0600

    Add files via upload

--- Lesson jQuery (Task Tracker project and jQuery)/scripts/jquery-serialization.js
+++ Lesson jQuery (Task Tracker project and jQuery)/scripts/jquery-serialization.js
@@ -1,22 +1,44 @@
-(function($) {
-    $.fn.extend({
-        toObject: () => {
-            var result = {}
-            this.serializeArray().map((obj) => {
-                result[obj.name] = obj.value;
-            })
-            return result;
-        },
-        fromObject: (obj) => {
-
-            this.find(':input').map((i, n) => {
-                var name = $(n).attr('name');
-                if (obj[name]) {
-                    $(n).val(obj[name]);
-                } else {
-                    $(n).val('');
-                }
-            })
+/*
+(function ($) {
+  $.fn.extend({
+    toObject: function () {
+      var result = {};
+      $.each(this.serializeArray(), function (i, v) {
+        result[v.name] = v.value;
+      });
+      return result;
+    },
+    fromObject: function (obj) {
+      $.each(this.find(":input"), function (i, v) {
+        var name = $(v).attr("name");
+        if (obj[name]) {
+          $(v).val(obj[name]);
+        } else {
+          $(v).val("");
         }
-    });
+      });
+    },
+  });
+})(jQuery);
+*/
+(function ($) {
+  $.fn.extend({
+    toObject: function () {
+      var result = {};
+      this.serializeArray().map((item) => {
+        result[item.name] = item.value;
+      });
+      return result;
+    },
+    fromObject: function (obj) {
+      this.find(":input").map((v) => {
+        var name = $(v).attr("name");
+        if (obj[name]) {
+          $(v).val(obj[name]);
+        } else {
+          $(v).val("");
+        }
+      });
+    },
+  });
 })(jQuery);
